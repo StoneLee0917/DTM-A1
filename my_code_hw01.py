2f13020814d98725aef551076553d845dc981996
stone <lisitong0917@outlook.com>
2022-11-20 15:56:44 +0100

    need to study spicy.spacial and KDtree and NNR

--- my_code_hw01.py
+++ my_code_hw01.py
@@ -4,13 +4,12 @@
 #-- [5683688]
 
 import random
-
+import math
 
 
 def nn_xy(dt, kd, all_z, x, y):
     """
     !!! TO BE COMPLETED !!!
-     
     Function that interpolates with nearest neighbour method.
      
     Input:
@@ -26,11 +25,34 @@ def nn_xy(dt, kd, all_z, x, y):
     #-- kd-tree docs: https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html#scipy.spatial.KDTree
     #-- you are *not* allowed to use the function for the nn interpolation that I wrote for startinpy
     #-- you need to write your own code for this step
-    z = random.uniform(0, 100)
-    raise Exception("Outside convex hull")
+    n=len(dt.points)
+    dis=[]
+    count=0
+    cp = dt.closest_point(x, y)
+    pt=dt.get_point(cp)
+    z1=pt[2]
+    z=dt.points[cp][2]
+    assert z1==z
+    # for i in range(1,n-1,1):
+    #     x1=dt.points[i][0]
+    #     y1=dt.points[i][1]
+    #     dis[i]=math.sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y))
+    # min=dis[0]
+    # for i in range(n-1):
+    #     if min>=dis[i]:
+    #         min=dis[i]
+    #         count=i
+    # z=dt.points[count][2]
+    #print(x,y,dt.points[count][0],dt.points[count][1],dt.points[count][2])
+    #z = random.uniform(0, 100)
+    #if cp not in dt.convex_hull():
+    if not dt.is_inside_convex_hull(dt.points[cp][0], dt.points[cp][1]):
+        raise Exception("Outside convex hull")
+    #raise Exception("Outside convex hull")
     return z
 
-
+def dis(pt,x,y):
+    return math.sqrt((pt[0]-x)**2+(pt[1]-y)**2)
 def idw_xy(dt, kd, all_z, x, y, power, radius):
     """
     !!! TO BE COMPLETED !!!
@@ -51,6 +73,10 @@ def idw_xy(dt, kd, all_z, x, y, power, radius):
     """
     #-- kd-tree docs: https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html#scipy.spatial.KDTree
     z = random.uniform(0, 100)
+    kd
+
+
+
     raise Exception("Outside convex hull")
     raise Exception("No point in search radius")
     return z
@@ -75,8 +101,17 @@ def tin_xy(dt, kd, all_z, x, y):
     #-- startinpy docs: https://startinpy.rtfd.io/
     #-- you are *not* allowed to use the function for the TIN interpolation that I wrote for startinpy
     #-- you need to write your own code for this step
-    z = random.uniform(0, 100)
-    raise Exception("Outside convex hull")
+    tri=dt.locate(x, y)
+    p1=dt.get_point(tri[0])
+    p2=dt.get_point(tri[1])
+    p3=dt.get_point(tri[2])
+    w1 = 0.5 * (p1[0] * p2[1] - p2[0] * p1[1] + p2[0] * y - x * p2[1] + x * p1[1] - p1[0] * y)
+    w2 = 0.5 * ((x * p2[1] - p2[0] * y) + (p2[0] * p3[1] - p3[0] * p2[1]) + (p3[0]*y - p3[1] * x))
+    w3 = 0.5 * ((p1[0] * y - x * p1[1]) + (p3[1] * x - y * p3[0]) + (p3[0] * p1[1] - p1[0] * p3[1]))
+    z=(w2*p1[2]+w3*p2[2]+w1*p3[2])/(w1+w2+w3)
+   # z = random.uniform(0, 100)
+    if not dt.is_inside_convex_hull(x, y):
+        raise Exception("Outside convex hull")
     return z
 
 
